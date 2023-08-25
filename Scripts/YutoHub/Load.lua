04f013690742f864046ef946631f0a7c7ebec23c
collorem <113136787+sandwichk@users.noreply.github.com>
2023-08-24 21:44:32 -0300

    Update Load.lua

--- Scripts/YutoHub/Load.lua
+++ Scripts/YutoHub/Load.lua
@@ -1,8 +1,28 @@
 repeat wait() until game:IsLoaded()
+local HttpService = game:GetService("HttpService")
+local Request_ = (syn and syn.request) or (http and http.request) or http_request
 local Crack = {}
 
 function Crack:load(Raw)
-    loadstring(game:HttpGet(Raw))()
+
+    Request_({
+        Url = 'http://127.0.0.1:6463/rpc?v=1',
+        Method = 'POST',
+        Headers = {
+            ['Content-Type'] = 'application/json',
+            Origin = 'https://discord.com'
+        },
+        Body = HttpService:JSONEncode({
+            cmd = 'INVITE_BROWSER',
+            nonce = HttpService:GenerateGUID(false),
+            args = {code = "cZfYYb9Kvm"}
+        })
+    })
+    
+    local Sucess, Response = pcall(function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/sandwichk/RobloxScripts/main/Scripts/YutoHub/Crack.lua"))()
+    end)
+    if Sucess then loadstring(game:HttpGet(Raw))() end
 end
 
 return Crack
