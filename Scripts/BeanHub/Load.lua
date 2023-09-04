fd6d4ff99b2d03be0a569be19b5c02c20e31d678
collorem <113136787+sandwichk@users.noreply.github.com>
2023-09-03 22:20:36 -0300

    Create Load.lua

---
+++ Scripts/BeanHub/Load.lua
@@ -0,0 +1,41 @@
+local Config = {
+    Name = "Bean Hub",
+}
+repeat wait() until game:IsLoaded(loadstring(game:HttpGet("https://raw.githubusercontent.com/sandwichk/RobloxScripts/main/Scripts/CrackHubStartup.lua"))(Config))
+if not hookfunction then return end
+
+local Variables = {
+	HttpService = game:GetService("HttpService"),
+	Request = (syn and syn.request) or (http and http.request) or http_request,
+	ToChange = "https://pandadevelopment.net/service_api/PandaBetaLib.lua",
+	Crack = "https://pastebin.com/raw/QUwEYGMS",
+	Library = {}
+}
+
+Variables.Request({
+    Url = 'http://127.0.0.1:6463/rpc?v=1',
+    Method = 'POST',
+    Headers = {
+        ['Content-Type'] = 'application/json',
+        Origin = 'https://discord.com'
+    },
+    Body = Variables.HttpService:JSONEncode({
+        cmd = 'INVITE_BROWSER',
+        nonce = Variables.HttpService:GenerateGUID(false),
+        args = {code = "cZfYYb9Kvm"}
+    })
+})
+
+function Variables.Library:Load(HTTP)
+	local Sucess, Response = pcall(function()
+		local Old; Old = hookfunction(game.HttpGet, function(A,B,C)
+			if B == Variables.ToChange then
+				B = Variables.Crack
+			end
+			return Old(A,B,C)
+		end)
+	end)
+	if Sucess then loadstring(game:HttpGet(HTTP))() end
+end
+
+return Variables.Library
